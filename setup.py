36edc73c1cc5ea01cd11e17ff86e503176f57cd2
Greg Doermann <gdoermann@gmail.com>
2010-08-04 12:38:01 -0600

    small change to setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers = ['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
                    'Framework :: Django'],
