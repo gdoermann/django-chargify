1958888d1727c586b050a4b0a72c80fb7eceb352
Greg Doermann <gdoermann@gmail.com>
2010-08-04 12:27:09 -0600

    Adding a setup.py file.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = (1, 0, 0)
+
+# Dynamically calculate the version based on VERSION tuple
+if len(VERSION)>2 and VERSION[2] is not None:
+    str_version = "%d.%d_%s" % VERSION[:3]
+else:
+    str_version = "%d.%d" % VERSION[:2]
+
+version= str_version
+
+setup(
+    name = 'django-chargify',
+    version = version,
+    description = "chargify",
+    long_description = """This is a generic SDK for hooking up with the Chargify API""",
+    author = 'Greg Doermann',
+    author_email = 'gdoermann@snirk.com',
+    url = 'http://github.com/gdoermann/django-chargify',
+    license = 'GNU General Public License',
+    platforms = ['any'],
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Framework :: Django'],
+    packages = find_packages(),
+    include_package_data = True,
+)
